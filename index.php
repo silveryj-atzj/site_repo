ebf2a83017ac9681591810c4151ee35113d2da88
银 <adriantan05@outlook.com>
2026-01-29 16:32:34 +0800

    a little changes

--- index.php
+++ index.php
@@ -1,2 +1,16 @@
-test
-<a href="/index.html">Example Link</a>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>银橘の站</title>
+</head>
+
+<body>
+    <h1>欢迎来到银橘の站</h1>
+
+    <h1>Hello, Auto Deploy Testing</h1>\
+
+    <h1>JIA CHEN</h1>
+    <a href="Furina @salgu.jpg">Example Link</a>
+</body>
